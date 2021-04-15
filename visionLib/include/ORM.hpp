dbf3e0ac10e12c960b7cc04d29b3435d6e1354a2
Ilyagu <inagdimaev@mail.ru>
2021-04-15 06:25:49 +0300

    added mocks [#1 #4]

--- visionLib/include/ORM.hpp
+++ visionLib/include/ORM.hpp
@@ -9,36 +9,105 @@
 #ifndef VISIONLIB_INCLUDE_ORM_HPP_
 #define VISIONLIB_INCLUDE_ORM_HPP_
 
+#include <ctime>
+#include <iostream>
 #include <string>
+#include <vector>
+// #include <pqxx/pqxx>
 
-class ORM {
+class ClientDB {
  private:
-  class ClientDB {
-   public:
-   private:
-    uint64_t ID;
-    std::string login;
-    std::string password;
-    uint64_t* passes;
-  };
-  class PassDB {
-   public:
-   private:
-    uint64_t ID;
-    std::string privateKey;
-    uint64_t companyID;
-  };
-  class CompanyDB {
-   public:
-   private:
-    uint64_t ID;
-    std::string name;
-  };
+  uint64_t ID;
+  std::string login;
+  std::string password;
+  uint64_t* passes;
 
  public:
-  PassDB getPassData(uint64_t ID);
-  ClientDB getClientData(uint64_t ID);
-  CompanyDB getCompanyData(uint64_t ID);
+};
+
+class PassDB {
+ private:
+  uint64_t ID;
+  std::string privateKey;
+  uint64_t companyID;
+
+ public:
+};
+
+class CompanyDB {
+ private:
+  uint64_t ID;
+  std::string name;
+
+ public:
+};
+
+class PassageDB {
+ private:
+  uint64_t ID;
+  time_t time;
+  uint8_t actionType;
+  uint64_t PassID;
+
+ public:
+};
+
+class DataBaseConnection {
+ public:
+  virtual bool logIn(std::string username, std::string password) {
+    return true;
+  }
+  virtual bool logOut(std::string username) { return true; }
+  virtual bool login(std::string username, std::string password) {
+    return true;
+  }
+
+ private:
+};
+
+class DataBase {
+ public:
+  // Конектится к базе данных, создается объет таблицы
+  DataBase();
+
+  ~DataBase();
+
+  bool insertPass(PassDB& PassID);
+  bool insertPassage(PassageDB& PassageID);
+  bool insertClient(ClientDB& ClientID);
+  bool insertCompany(CompanyDB& CompanyID);
+
+  bool deletePass(uint64_t PassID);
+  bool deletePassage(uint64_t PassageID);
+  bool deletePass(uint64_t CompanyID, uint64_t ClientID);
+  bool deleteClient(uint64_t ClientID);
+  bool deleteCompany(uint64_t CompanyID);
+
+  bool PassExists(uint64_t PassID);
+  bool ClientExists(uint64_t ClientID);
+  bool CompanyExists(uint64_t CompanyID);
+  bool PassageExists(uint64_t PassageID);
+
+  std::vector<PassDB> getAllPasses(const std::string& sql_limit,
+                                   const std::string& sql_offset);
+  std::vector<PassageDB> getAllPassages(const std::string& sql_limit,
+                                        const std::string& sql_offset);
+  std::vector<ClientDB> getAllClients(const std::string& sql_limit,
+                                      const std::string& sql_offset);
+  std::vector<CompanyDB> getAllCompanys(const std::string& sql_limit,
+                                        const std::string& sql_offset);
+  std::vector<PassDB> getClientsPasses(const uint64_t ClientID);
+  std::vector<PassDB> getCompanysPasses(const uint64_t CompanyID);
+  std::vector<PassageDB> getPassesPassages(const uint64_t PassID);
+
+  PassDB getPass(std::string PassID);
+  PassDB getPassage(std::string PassageID);
+  ClientDB getClient(std::string ClientID);
+  CompanyDB getCompany(std::string CompanyID);
+
+ private:
+  void do_modifying_request(const std::string& sql_request);
+  // pqxx::result do_select_request(const std::string& sql_request);
 };
 
 #endif  // VISIONLIB_INCLUDE_ORM_HPP_
--- visionLib/source/ORM.cpp
+++ visionLib/source/ORM.cpp
@@ -8,10 +8,101 @@
 
 #include "../include/ORM.hpp"
 
-#include <iostream>
+bool insertPass(PassDB& PassID) {
+    return true;
+}
+bool insertPassage(PassageDB& PassageID){
+    return true;
+}
+bool insertClient(ClientDB& ClientID){
+    return true
+}
 
-ORM::ClientDB ORM::getClientData(uint64_t ID) { return ClientDB(); }
+bool insertCompany(CompanyDB& CompanyID){
+    return true;
+}
 
-ORM::CompanyDB ORM::getCompanyData(uint64_t ID) { return CompanyDB(); }
 
-ORM::PassDB ORM::getPassData(uint64_t ID) { return PassDB(); }
+bool deletePass(uint64_t PassID){
+    return true;
+}
+
+bool deletePassage(uint64_t PassageID){
+    return true;
+}
+
+bool deletePass(uint64_t CompanyID, uint64_t ClientID){
+    return true;
+}
+
+bool deleteClient(uint64_t ClientID){
+    return true;
+}
+
+bool deleteCompany(uint64_t CompanyID){
+    return true;
+}
+
+bool PassExists(uint64_t PassID){
+    return true;
+}
+
+bool ClientExists(uint64_t ClientID){
+    return true;
+}
+
+bool CompanyExists(uint64_t CompanyID){
+    return true;
+}
+
+bool PassageExists(uint64_t PassageID){
+    return true;
+}
+
+std::vector<PassDB> getAllPasses(const std::string& sql_limit, const std::string& sql_offset){
+    std::vector<PassDB> passes(0);
+    return passes;
+}
+
+std::vector<PassageDB> getAllPassages(const std::string& sql_limit, const std::string& sql_offset){
+    std::vector<PassageDB> passes(0);
+    return passes;
+}
+
+std::vector<ClientDB> getAllClients(const std::string& sql_limit, const std::string& sql_offset){
+    std::vector<ClientDB> passes(0);
+}
+
+std::vector<CompanyDB> getAllCompanys(const std::string& sql_limit, const std::string& sql_offset){
+    std::vector<CompanyDB> passes(0);
+}
+
+std::vector<PassDB> getClientsPasses(const uint64_t ClientID){
+    std::vector<PassDB> passes(0);
+}
+
+std::vector<PassDB> getCompanysPasses(const uint64_t CompanyID){
+    std::vector<PassDB> passes(0);
+}
+
+std::vector<PassageDB> getPassesPassages(const uint64_t PassID){
+    std::vector<PassageDB> passes(0);
+}
+
+
+
+PassDB getPass(std::string PassID){
+
+}
+
+PassDB getPassage(std::string PassageID){
+
+}
+
+ClientDB getClient(std::string ClientID){
+
+}
+
+CompanyDB getCompany(std::string CompanyID){
+
+}
