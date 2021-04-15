75ff910f41a70ed4b7291aad90cd4fbb3670b3ee
Semyon Zaharchenko <felix.wojchik@gmail.com>
2021-04-15 17:24:41 +0300

    remove network/mock/readTemp testes / progress [#1]

---
+++ visionLib/include/ClientRequest.hpp
@@ -0,0 +1,19 @@
+// Copyright 2021 Monkeys. All rights reserved.
+//  __  __                   _  __
+// |  \/  |   ___    _ __   | |/ /   ___   _   _   ___
+// | |\/| |  / _ \  | '_ \  | ' /   / _ \ | | | | / __|
+// | |  | | | (_) | | | | | | . \  |  __/ | |_| | \__ \
+// |_|  |_|  \___/  |_| |_| |_|\_\  \___|  \__, | |___/
+//                                         |___/
+
+#ifndef VISIONLIB_INCLUDE_TERMINALREQUEST_HPP_
+#define VISIONLIB_INCLUDE_TERMINALREQUEST_HPP_
+
+class TerminalRequest {
+public:
+    void waitRequest();
+    void waitConnection();
+    void reply();
+};
+
+#endif  // VISIONLIB_INCLUDE_TERMINALREQUEST_HPP_
