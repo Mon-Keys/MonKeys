87cac63852fd5e109550969eeed2b092e2892ff8
leonid perlin <56253167+perlinleo@users.noreply.github.com>
2021-04-15 16:14:51 +0300

    Update ClientRequest.cpp

--- visionLib/source/ClientRequest.cpp
+++ visionLib/source/ClientRequest.cpp
@@ -1,17 +1,1 @@
-// Copyright 2021 Monkeys. All rights reserved.
-//  __  __                   _  __
-// |  \/  |   ___    _ __   | |/ /   ___   _   _   ___
-// | |\/| |  / _ \  | '_ \  | ' /   / _ \ | | | | / __|
-// | |  | | | (_) | | | | | | . \  |  __/ | |_| | \__ \
-// |_|  |_|  \___/  |_| |_| |_|\_\  \___|  \__, | |___/
-//                                         |___/
 
-#include "../include/ClientRequest.hpp"
-
-void ClientRequest::initServer() {}
-void ClientRequest::initLogger() {}
-void ClientRequest::setLoggerLevel() {}
-void ClientRequest::waitConnection() {}
-void ClientRequest::waitRequest() {}
-void ClientRequest::runHandler() {}
-void ClientRequest::reply() {}
