cf77926506c7a0cf1e7d186748918145f17a3b9d
Pierrelean @bolshoyshlyoppa aka Leonid Perlin Vadimovich <perlinleo@gmail.com>
2021-04-15 16:27:49 +0300

    TimeCodeGenerator.hpp

---
+++ visionLib/include/TimeCodeGenerator.hpp
@@ -0,0 +1,52 @@
+// Copyright 2021 Monkeys. All rights reserved.
+//  __  __                   _  __
+// |  \/  |   ___    _ __   | |/ /   ___   _   _   ___
+// | |\/| |  / _ \  | '_ \  | ' /   / _ \ | | | | / __|
+// | |  | | | (_) | | | | | | . \  |  __/ | |_| | \__ \
+// |_|  |_|  \___/  |_| |_| |_|\_\  \___|  \__, | |___/
+//                                         |___/
+
+#ifndef VISIONLIB_INCLUDE_TIMECODEGENERATOR_HPP_
+#define VISIONLIB_INCLUDE_TIMECODEGENERATOR_HPP_
+
+#include <iostream>
+#include <string>
+
+class TimeCodeGenerator {
+ private:
+  std::string privateKey;
+  uint64_t PassID;
+  uint64_t timeStamp;
+  uint16_t timeInterval;
+
+  // startTime --- UNIX0
+
+  std::string getHashedOTP();
+  uint64_t getTParameter();
+  void updateTime();
+
+ public:
+  TimeCodeGenerator(std::string privateKey, uint64_t PassID,
+                    uint16_t timeInterval, uint64_t timeStamp);
+  std::string generateTimeCode();
+  TimeCodeGenerator();
+  ~TimeCodeGenerator();
+  TimeCodeGenerator(const TimeCodeGenerator&) = delete;
+  TimeCodeGenerator(TimeCodeGenerator&&) = default;
+};
+
+class TimeCode {
+private:
+  uint64_t CompanyID;
+  uint64_t PassID;
+  std::string timePart;
+  uint64_t decodeFrom64(std::string toDecode);
+public:
+  explicit TimeCode(std::string timecodeString);
+  ~TimeCode();
+  uint64_t getCompanyID();
+  uint64_t getPassID();
+  std::string getTimePart();
+};
+
+#endif  // VISIONLIB_INCLUDE_TIMECODEGENERATOR_HPP_
